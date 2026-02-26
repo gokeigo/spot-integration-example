a4311bd5c1b2518e7ce2a7c4aaaa76e538ccb7eb
Martin <borquezmartin@uc.cl>
2026-02-26 17:07:21 -0300

    🔧 Refactor showModalAtom to use a boolean atom instead of storage

--- src/atoms/simulation-settings.ts
+++ src/atoms/simulation-settings.ts
@@ -1,11 +1,9 @@
+import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import { env } from "~/env";
 
 export const publicKeyAtom = atomWithStorage<string>("gokei-public-key", "");
-export const showModalAtom = atomWithStorage<boolean>(
-  "gokei-show-modal",
-  !env.NEXT_PUBLIC_GOKEI_PUBLIC_KEY,
-);
+export const showModalAtom = atom<boolean>(true);
 export const integrationTypeAtom = atomWithStorage<string>(
   "gokei-integration-type",
   "modal",
